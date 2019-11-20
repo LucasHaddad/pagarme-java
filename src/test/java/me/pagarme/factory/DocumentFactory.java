d71931f9c1f28f9a7f69233a03171571b08aabfa
lucashaddad <lucas.haddad@teknisa.com>
2019-11-20 17:01:33 -0300

    Change cpf for a valid cpf.

--- src/test/java/me/pagarme/factory/DocumentFactory.java
+++ src/test/java/me/pagarme/factory/DocumentFactory.java
@@ -7,10 +7,10 @@ import me.pagar.model.Document;
 
 public class DocumentFactory {
     public static final Document.Type DEFAULT_TYPE = Document.Type.CPF;
-    public static final String DEFAULT_NUMBER = "11111111111";
+    public static final String DEFAULT_NUMBER = "78524455004";
 
     public Collection<Document> create() {
-        Collection<Document> documents = new ArrayList<Document>();
+        Collection<Document> documents = new ArrayList<>();
         Document document = new Document(DEFAULT_TYPE, DEFAULT_NUMBER);
 
         documents.add(document);
